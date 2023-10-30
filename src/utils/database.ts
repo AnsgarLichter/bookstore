34ec831a10896fe5811a0e14466196a32bb83152
Ansgar Lichter <lichteransgar@gmail.com>
2023-10-30 09:02:56 +0100

    refactor: use env to get database url

--- src/utils/database.ts
+++ src/utils/database.ts
@@ -1,14 +1,19 @@
 import mongoose from "mongoose";
-
-const connectionUrl = "mongodb+srv://lichteransgar:qNXAYYqRQEpM5poI@cluster0.fahxqjk.mongodb.net/?retryWrites=true&w=majority";
+import logger from "../middleware/logger.middleware";
 
 export default class Database {
     public static async connect() {
+        const connectionUrl = process.env.database_url;
+        if (!connectionUrl) {
+            logger.error("The url to connect to the database is not defined!");
+            throw new Error("Database URL not defined");
+        }
+
         try {
             await mongoose.connect(connectionUrl);
-            logger.error("Successfully connected to MongoDB.");
+            logger.info("Successfully connected to MongoDB.");
         } catch (error) {
-            console.error(`Connection to MongoDB failed: ${error}`);
+            logger.error(`Connection to MongoDB failed: ${error}`);
             throw error;
         }
     }
