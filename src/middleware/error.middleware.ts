2bffe5ef451382684888159199ff57f36ebfd58c
Ansgar Lichter <lichteransgar@gmail.com>
2023-10-30 09:51:00 +0100

    feat: add logging

--- src/middleware/error.middleware.ts
+++ src/middleware/error.middleware.ts
@@ -1,6 +1,7 @@
 import { Request, Response, NextFunction } from "express";
 
 import HttpError from "../utils/httpError.error"
+import logger from "./logger.middleware";
 
 const errorMiddleware = (
     error: HttpError,
@@ -10,10 +11,12 @@ const errorMiddleware = (
 ): void => {
     const statusCode = error.statusCode || 500;
     const message = error.message || "An error occurred!";
-
+    
+    logger.info(`Caught http error with statusCode ${statusCode} and message ${message}.`);
     response.status(statusCode).json({
         message: message
     });
+    logger.info(`Applied error properties to the response!`);
 }
 
 export default errorMiddleware;
